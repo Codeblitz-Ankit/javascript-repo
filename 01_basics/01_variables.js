9092838136c768cf30c35192358caba2b59e79f8
Ankit S K <01fe23bcs127@kletech.ac.in>
2025-10-17 11:31:22 +0000

    variables learn

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 24578
+let accountEmail = "Penguin@gmail.com"
+var accountPassword = "547821"
+accountCity = "Hubballi"
+
+// accountId = 457
+
+console.log(accountId);
+console.log(accountEmail);
+
+
+/*do not use var because of block scope*/
